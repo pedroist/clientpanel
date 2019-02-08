4b6d3d04d53830e8de435b01b2726a36f2310460
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-02-08 01:14:05 +0000

    Section 10 - Login Authentication

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/']);
+      }
+    })
   }
 
+  onSubmit() {
+    /*email: brad@gmail.com
+      password: 123456
+    */
+    this.authService.login(this.email, this.password)
+      .then(
+        res => {
+          //flashMessage
+          this.flashMessage.show("You are now logged in", {
+            cssClass: 'alert-success', timeout: 3000
+          });
+          //redirect to Dashboard
+          this.router.navigate(['/']);
+        })
+      .catch(err => {
+        this.flashMessage.show(err.message, {
+          cssClass: 'alert-danger', timeout: 3000
+        });
+      });
+  }
 }
