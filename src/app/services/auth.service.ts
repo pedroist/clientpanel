fc4d1df39240bca3d3df59b45d8a37dea738d6df
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-02-08 02:55:54 +0000

    Section 10 - User Registration

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,6 +19,16 @@ export class AuthService {
     });
   }
 
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+        .then(
+          userData => resolve(userData),
+          err => reject(err)
+        );
+    });
+  }
+
   //Check if we're logged in
   getAuth() {
     return this.afAuth.authState;
